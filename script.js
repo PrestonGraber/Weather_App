24209f0c18ad221d8548c2839b918ff7d7392205
PrestonGraber <prestongraber123@gmail.com>
2022-05-11 15:11:44 -0400

    commit

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const wrapper = document.querySelector(".weather-data"),
 inputpart = document.querySelector(".input-part"),
 inputField = inputpart.querySelector("input"),
 locationBtn = inputpart.querySelector("button");
+
+wIcon = document.querySelector(".weather-img img");
+const element = document.querySelector(".container");
+
 let api;
+
+
 const apiKey = "d66509e2ed85bc99e3fb95ba6d7759b8";
 function geoloc(){
    if(navigator.geolocation){
@@ -13,7 +19,7 @@ function geoloc(){
 }
 function onSuccess(position){
     const {latitude,longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
     fetchData();
 }
 function onError(error){
@@ -29,7 +35,7 @@ inputField.addEventListener("keyup", e => {
    
 });
 function requestApi(city){
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     fetch(api).then(response => response.json()).then(result => weatherDetails(result));
 }
 function fetchData(){
@@ -40,17 +46,55 @@ function weatherDetails(info){
         alert("City not found.")
     }else{
         const city = info.name;
-        const country = info.sys.country;
+
         const {description,id} = info.weather[0];
         const{temp, humidity,pressure} = info.main;
         const {speed} = info.wind;
         const {all} = info.clouds;
-        if(info.clouds.all >= 55){
-            
+        const {sunrise,sunset} = info.sys;
+        let dateObj = new Date(sunrise * 1000);
+        let dateObj2 = new Date(sunset * 1000);
+ 
+        // Get hours from the timestamp
+        let hours = dateObj.getUTCHours()-4;
+        let hours2 = dateObj2.getUTCHours()+8;
+        // Get minutes part from the timestamp
+        let minutes = dateObj.getUTCMinutes();
+        let minutes2 = dateObj2.getUTCMinutes();
+        
+        let formattedTime = hours.toString().padStart(2, '0') + ':' + 
+                minutes.toString().padStart(2, '0') + ':' ;
+
+        let formattedTime2 = hours2.toString().padStart(2, '0') + ':' + 
+                minutes2.toString().padStart(2, '0') + ':' ;
+       
+
+        if(id == 800){
+            wIcon.src = "photos/clear.png";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "photos/storm.png";
+            document.getElementsByClassName('container')[0].style.backgroundImage="url(photos/background-night.webp)";
+        }
+        else if(id >= 600 && id <= 622){
+            wIcon.src = "photos/snow.png";
+        }
+        else if(id >= 701 && id <= 781){
+            wIcon.src = "photos/haze.png";
+        }
+        else if(id >= 801 && id <= 804){
+            wIcon.src = "photos/cloud.png";
+           
+        }
+        else if((id >=300 && id<=321) || (id >=500 && id <= 531)){
+            wIcon.src = "photos/storm.png"
         }
         wrapper.querySelector(".location .location-label").innerText = city;
-        wrapper.querySelector(".temperature .temper").innerText = temp;
+        wrapper.querySelector(".temperature .temper").innerText = Math.floor(temp);
         wrapper.querySelector(".temperature .humid").innerText = humidity;
+
+        wrapper.querySelector(".future .time").innerText = formattedTime;
+        wrapper.querySelector(".future .time-night").innerText = formattedTime2;
+
         wrapper.querySelector(".param .wind").innerText = speed;
         wrapper.querySelector(".param .cloud").innerText = all;
         wrapper.querySelector(".param .press").innerText = pressure;
